114a1f234281b2e23531f30b2965af923cf40976
nouraldeen28 <nouraldeenalsaadi@gmail.com>
2024-08-31 21:04:36 +0300

    added delte for dashboard card items (ClosingRates only for now)

--- src/components/sales/CardsMenu.vue
+++ src/components/sales/CardsMenu.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { defineEmits, defineProps, onMounted, ref, watch, computed, toRef } from 'vue'
 import AddClosingRates from './AddClosingRates.vue'
+import DeleteCardItem from './DeleteCardItem.vue'
 import { useDashboardStore } from 'src/stores/dashboard'
 
 const dashboardStore = useDashboardStore()
@@ -8,6 +9,10 @@ const data = toRef(props, 'data')
 const props = defineProps(['data', 'type'])
 const emit = defineEmits(['itemsToshow'])
 const items = ref([])
+const deleteDialog = ref({
+    state: false,
+    item: null
+})
 function setSummaryItems() {
     const { calls, offers, sales } = data.value
     const filteredItems = { calls, offers, sales }
@@ -74,6 +79,13 @@ function setTotalUnitsItems() {
 function refresh() {
     dashboardStore.getDashboardData()
     addDialog.value = false
+    deleteDialog.value.state = false
+    deleteDialog.value.item = null
+}
+
+function deleteItem(item) {
+    deleteDialog.value.item = item
+    deleteDialog.value.state = true
 }
 const addDialog = ref(false)
 function setupMenu() {
@@ -119,6 +131,8 @@ watch(data,
     }
 )
 const disableCheckBox = computed(() => items.value.filter(item => item.value).length == 1)
+const deletable = computed(() => props.type == 'ClosingRates')
+
 </script>
 
 <template>
@@ -127,14 +141,17 @@ const disableCheckBox = computed(() => items.value.filter(item => item.value).le
         <q-menu class="bg-accent text-white" transition-show="jump-down" transition-hide="jump-up" fit
             v-if="items.length > 0">
             <q-list style="min-width: 100px">
-                <q-item class="q-pl-sm text-center" tag="label" v-ripple v-for="item in items"
-                    :key="'homeMenuItem' + item.label">
+                <q-item class="q-pl-sm text-center" :tag="deletable ? 'div' : 'label'" v-ripple="deletable ? true : false"
+                    v-for="item in items" :key="'homeMenuItem' + item.label" :clickable="true">
                     <q-item-section side top>
                         <q-checkbox v-model="item.value" color="red-10" :disable="disableCheckBox && item.value" />
                     </q-item-section>
 
                     <q-item-section>
                         <q-item-label class="non-selectable">{{ item.label }}</q-item-label>
+                    </q-item-section>
+                    <q-item-section side @click.stop="deleteItem(item)" v-if="deletable">
+                        <q-icon name="delete_forever" color="white" class="dashboard-menu-item-delete"></q-icon>
                     </q-item-section>
                 </q-item>
                 <q-separator />
@@ -152,7 +169,21 @@ const disableCheckBox = computed(() => items.value.filter(item => item.value).le
         <q-dialog v-model="addDialog">
             <AddClosingRates v-if="props.type == 'ClosingRates'" @submited="refresh()" />
         </q-dialog>
+
+        <q-dialog v-model="deleteDialog.state" persistent>
+            <DeleteCardItem :type="props.type" :item="deleteDialog.item" @deleted="refresh()"
+                @cancel="deleteDialog.state = false" />
+        </q-dialog>
     </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.dashboard-menu-item-delete {
+    transition-duration: 0.3s;
+    cursor: pointer;
+
+    &:hover {
+        color: #D40403 !important;
+    }
+}
+</style>
